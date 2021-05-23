884677bf8f561e96e9c33b7011fcfb71e4444713
Cuong UET <18020262@vnu.edu.vn>
2021-05-24 00:57:10 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
-import 'package:mobile_app/stores/user_store.dart';
-import 'package:provider/provider.dart';
-import 'package:mobile_app/routes.dart';
+import 'package:mobile_app/routes/route.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(debug: true);
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => UserStore()),
-      ],
-      child: MobileApp(),
-    ),
-  );
+  runApp(MobileApp());
 }
 
 class MobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Mobile App",
-      initialRoute: "/home",
-      routes: Routes.routes,
+    return new MaterialApp(
+      initialRoute: '/',
+      routes: getRouter(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  // hàm này chuyển hướng sang settings
+  void _navigateToSettings(BuildContext context) {
+    Navigator.pushNamed(context, '/settings');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        child: GridView.count(
+          childAspectRatio: (1.6 / 1),
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 16,
+          crossAxisCount: 2,
+          // GridView này chia mỗi hàng làm 2 grid - chưa được 2 widget với tỉ lệ rộng/dài của mỗi Grid là 1.6/1
+          // khoảng cách chiều dọc của 2 grid với nhau là 10, chiều ngang là 2
+          children: [],
+        ),
+        decoration: BoxDecoration(
+        )),
+      ),
     );
   }
 }
